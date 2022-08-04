eedbf84606b1dd490c602111cfba5cd044dececa
Tuan Le <lethanhtuan1028@gmail.com>
2022-08-04 16:59:33 +0700

    update source code

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 const PORT = 8080;
 
 var router = express.Router();
 
-router.get('/', function (request, response) {
-  response.render('index', { title: 'Welcome!' });
+router.get("/", function (request, response) {
+  response.render("index", { title: "Welcome!" });
 });
 
-router.get('/student', function (request, response) {
-  response.render('index', { title: 'Welcome, student!' });
+router.post("/contact", function (request, response) {
+  console.log(request.body);
 });
 
-router.get('/teacher', function (request, response) {
-  response.render('index', { title: 'Welcome, teacher!' });
-});
-
-app.use('/', router);
+app.use("/", router);
 
 app.listen(PORT, function () {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
+
+module.exports = app;
---
+++ lambda.js
@@ -0,0 +1,17 @@
+"use strict";
+const awsServerlessExpress = require("aws-serverless-express");
+const warmer = require("lambda-warmer");
+
+const app = require("./app");
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => {
+  // Start a promise chain
+  warmer(event).then((isWarmer) => {
+    // If a warming event
+    if (isWarmer) return "warmed";
+
+    // Proceed with handler logic
+    awsServerlessExpress.proxy(server, event, context);
+  });
+};
